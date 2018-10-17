e729772fd34573e8be6cd2935fb6b67787a04291
yoshinoniwa <yoshinoniwa>
2018-10-17 11:05:19 +0900

    ログファイルの追加

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class Main {
 	File csv_file;
-	String file_nama = "";
+	String file_nama = "./traffic_log/16-10-08.csv";
 	
 	public static void main(String args[]){
 		
