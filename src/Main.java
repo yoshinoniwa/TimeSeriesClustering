42094b8149c62cf49ba5c56d84cbc72c3dfc22ee
yoshinoniwa <yoshinoniwa>
2018-10-25 17:14:53 +0900

    平均算出

--- src/Main.java
+++ src/Main.java
@@ -4,85 +4,113 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 	// --------------ファイル読み込み用--------------
-	// ファイル
-	static File csv_file;
-	static String csv_file_name = "./traffic_log/slide_30_window_60.arff";
-	// オリジナルのcsvデータを格納するリスト
-	static ArrayList<String[]> file_list = new ArrayList<String[]>();
-	// ダブルクォーテーションを無くしたcsvファイル
-	static String[][] csv_data = new String[file_list.size()][];
-	// ダブルクォーテーションを無くしたcsvファイルのリスト
-	static ArrayList<String[]> csv_data_list = new ArrayList<String[]>();
-
-	// arff ファイル読み込み用
-	static File arff_file;
-	static String arff_file_name = "./traffic_log/slide_30_window_60.txt";
-	// オリジナルのcsvデータを格納するリスト
+	static File arff_file = new File("./traffic_log/slide_30_window_180.txt");
 	static ArrayList<String[]> arff_file_list = new ArrayList<String[]>();
-	// ダブルクォーテーションを無くしたcsvファイル
-	static String[][] arff_data = new String[file_list.size()][];
-	// ダブルクォーテーションを無くしたcsvファイルのリスト
-	static ArrayList<String[]> arff_data_list = new ArrayList<String[]>();
+	static String[][] arff_data;
+	// クラスタ0のデータの平均と分散
+	static ArrayList<Long> cluster0_data_ave = new ArrayList<Long>();
+	static ArrayList<Double> cluster0_data_var = new ArrayList<Double>();
+	// クラスタ1のデータの平均と分散
+	static ArrayList<Long> cluster1_data_ave = new ArrayList<Long>();
+	static ArrayList<Double> cluster1_data_var = new ArrayList<Double>();
+	// クラスタ2のデータの平均と分散
+	static ArrayList<Long> cluster2_data_ave = new ArrayList<Long>();
+	static ArrayList<Double> cluster2_data_var = new ArrayList<Double>();
+
+	static long data_ave;
+	static double data_var;
 
 	// メインメソッド
-	public static void main(String args[]) throws FileNotFoundException {
+	public static void main(String args[]) {
 		callFile();
-//		csv_data = toStringArray();
-//		System.out.println(csv_data[10][1]);
-		System.out.println(arff_data[10][1]);
+		DivideCluster(arff_data);
+		Average(cluster0_data_ave);
+		Average(cluster1_data_ave);
+		Average(cluster2_data_ave);
+		Variance(cluster0_data_var);
+		Variance(cluster1_data_var);
+		Variance(cluster2_data_var);
 	}
 
 	// ファイル読み込み
-	public static void callFile() throws FileNotFoundException {
+	public static void callFile() {
 		try {
-			csv_file = new File(csv_file_name);
-			BufferedReader br = new BufferedReader(new FileReader(csv_file));
-			// ArrayList:filelistにCSVファイルを格納
-			while (br.ready()) {
-				String line = br.readLine();
-				file_list.add(line.split(",")); // ,で区切って読み込み
+			// ファイル読み込み
+			BufferedReader br = new BufferedReader(new FileReader(arff_file));
+			String line = br.readLine();
+			for (int row = 0; line != null; row++) {
+				arff_file_list.add(line.split(",", 0));
+				line = br.readLine();
 			}
-			//arff file 読み込み
-			arff_file = new File(arff_file_name);
-			BufferedReader br_arff = new BufferedReader(new FileReader(arff_file));
-			// ArrayList:filelistにCSVファイルを格納
-			while (br_arff.ready()) {
-				String line = br_arff.readLine();
-				file_list.add(line.split(",")); // ,で区切って読み込み
+			br.close();
+			arff_data = new String[arff_file_list.size()][];
+			// arrayListから二次元配列へ
+			for (int row = 0; row < arff_file_list.size(); row++) {
+				arff_data[row] = arff_file_list.get(row);
+				for (int col = 0; col < 4; col++) {
+					// System.out.println(arff_data[row][col]);
+				}
 			}
 		} catch (IOException e) {
 			System.out.println(e);
 		}
 	}
 
-//	// ファイルのダブルクォーテーションを消すメソッド
-//	public static String[][] toStringArray() {
-//		// data[][]のなかにダブルクォーテーションを消した値を入れる
-//		String[][] data = new String[file_list.size()][];
-//		// fileDbleQuoはArrayListから二次元配列にするための仮の配列
-//		String[][] fileDbleQuo = new String[file_list.size()][];
-//		// dataの配列に値を入れるための仮置場
-//		String dbleQuo;
-//		for (int i = 0; i < file_list.size(); i++) {
-//			fileDbleQuo[i] = file_list.get(i);
-//			// ArrayListから二次元配列
-//			data[i] = file_list.get(i);
-//			for (int k = 0; k < 5; k++) {
-//				dbleQuo = fileDbleQuo[i][k];
-//				data[i][k] = dbleQuo.replace("\"", "");// ダブルクォーテーションを消す
-//			}
-//			csv_data_list = extractCsvData(data[i]);
-//		}
-//		return data;
-//	}
-
-	public static ArrayList<String[]> extractCsvData(String[] data) {
-		ArrayList<String[]> csv_data_list = new ArrayList<String[]>();
-		csv_data_list.add(data);
-		return csv_data_list;
+	// クラスたごとに平均、分散を分ける
+	public static void DivideCluster(String[][] data) {
+		ArrayList<String> line_data = new ArrayList<String>();
+		int cluster_num;
+		Pattern pattern;
+		Matcher matcher;
+		for (int i = 0; i < arff_file_list.size(); i++) {
+			cluster_num = Integer.parseInt(data[i][3].replace("cluster", ""));
+//			System.out.println(cluster_num);
+			if (cluster_num == 0) {
+				cluster0_data_ave.add(Long.parseLong(data[i][1]));
+				cluster0_data_var.add(Double.parseDouble(data[i][2]));
+			} else if (cluster_num == 1) {
+				cluster1_data_ave.add(Long.parseLong(data[i][1]));
+				cluster1_data_var.add(Double.parseDouble(data[i][2]));
+			} else if (cluster_num == 2) {
+				cluster2_data_ave.add(Long.parseLong(data[i][1]));
+				cluster2_data_var.add(Double.parseDouble(data[i][2]));
+			}
+		}
 	}
+
+	public static void Average(ArrayList<Long> list) {
+		long ave;
+		long sum = 0;
+		if (list == null || list.size() == 0) {
+//			System.exit(0);
+		} else {
+			for (int i = 0; i < list.size(); i++) {
+				sum += list.get(i);
+			}
+			ave = sum / list.size();
+			System.out.println(ave);
+		}
+	};
+
+	public static void Variance(ArrayList<Double> list) {
+		double ave;
+		double sum = 0;
+		if (list == null || list.size() == 0) {
+//			System.exit(0);
+		} else {
+			for (int i = 0; i < list.size(); i++) {
+				sum += list.get(i);
+			}
+			ave = sum / list.size();
+			System.out.println(ave);
+		}
+	}
+
+	
 
 }
