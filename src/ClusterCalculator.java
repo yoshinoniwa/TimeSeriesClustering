4c637fada24ae3e063c0322115d3ea0b402efbbc
yoshinoniwa <yoshinoniwa>
2018-11-07 17:07:37 +0900

    udpデータ読み込み

--- src/ClusterCalculator.java
+++ src/ClusterCalculator.java
@@ -1,47 +1,115 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ClusterCalculator {
-	// クラスタ1のデータの平均と分散
-	static ArrayList<Long> cluster0_data_ave = new ArrayList<Long>();
-	static ArrayList<Double> cluster0_data_var = new ArrayList<Double>();
-	// クラスタ1のデータの平均と分散
-	static ArrayList<Long> cluster1_data_ave = new ArrayList<Long>();
-	static ArrayList<Double> cluster1_data_var = new ArrayList<Double>();
-	// クラスタ1のデータの平均と分散
-	static ArrayList<Long> cluster2_data_ave = new ArrayList<Long>();
-	static ArrayList<Double> cluster2_data_var = new ArrayList<Double>();
-	// クラスタ1のデータの平均と分散
-	static ArrayList<Long> cluster3_data_ave = new ArrayList<Long>();
-	static ArrayList<Double> cluster3_data_var = new ArrayList<Double>();
-	
-	static final String file_day = "16-09-30";
-	static final String file_name = "./traffic_log/"+file_day+".csv";
-	public static void CallFile(){
-		
+	static String[] traffic_data_set;
+	// UDPファイル
+	static final String udp_day = "16-09-27-udp"; // ファイル名
+	static final String udp_file_name = "./traffic_log/" + udp_day + ".csv"; // ファイルの場所
+	static File udp_file = new File(udp_file_name);// ファイル
+	static ArrayList<String[]> udp_file_list = new ArrayList<String[]>(); // csvファイルをArrayListに格納
+	static String[][] udp_data; // ArrayList→二次元配列
+	static ArrayList<Long[]> udp_traffic_sum_set = new ArrayList<Long[]>(); // プロトコル[UDP]のデータ量を格納
+	static ArrayList<Long> udp_traffic_sum_list;
+	static ArrayList<ArrayList> udp_traffic_sum_num = new ArrayList<ArrayList>();
+	static String[] udp_traffic_data_set;
+	// TCPファイル
+	static final String tcp_day = "16-09-27-tcp";
+	static final String tcp_file_name = "./traffic_log/" + tcp_day + ".csv";
+	static File tcp_file = new File(tcp_file_name);
+	static ArrayList<String[]> tcp_file_list = new ArrayList<String[]>();
+	static String[][] tcp_data;
+
+	// static
+	public static void CallFile() {
+		// UDPのファイル
+		try {
+			// ファイル読み込み
+			// オリジナルファイル
+			BufferedReader br = new BufferedReader(new FileReader(udp_file));
+			String line = br.readLine();
+			for (int row = 0; line != null; row++) {
+				udp_file_list.add(line.split(",", 0));
+				line = br.readLine();
+			}
+			br.close();
+
+			udp_data = new String[udp_file_list.size()][];
+
+			// arrayListから二次元配列へ
+			for (int row = 0; row < udp_file_list.size(); row++) {
+				udp_data[row] = udp_file_list.get(row);
+				for (int col = 0; col < 7; col++) {
+					udp_data[row][col] = udp_data[row][col].replace("\"", "");
+				}
+			}
+
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		// TCPのファイル
 	}
-	public static void DivideCluster(String[][] data, ArrayList list) {
-		ArrayList<String> line_data = new ArrayList<String>();
-		int cluster_num;
-		Pattern pattern;
-		Matcher matcher;
+
+	public static void DivideCluster(String[][] data,ArrayList list) {
+//		ArrayList<String> line_data = new ArrayList<String>();
+//
+//		String ip_address[] = new String[data.length];
+//		String file_data[][] = new String[data.length][4];
+
+		udp_traffic_data_set = new String[list.size()];
+		int count = 0;
+		int t_count = 0;
+		Long[] t_sum_num = new Long[list.size()];
+
+		String comparsion_str = "";
 		for (int i = 0; i < list.size(); i++) {
-			cluster_num = Integer.parseInt(data[i][3].replace("cluster", ""));
-			// System.out.println(cluster_num);
-			if (cluster_num == 0) {
-				cluster0_data_ave.add(Long.parseLong(data[i][1]));
-				cluster0_data_var.add(Double.parseDouble(data[i][2]));
-			} else if (cluster_num == 1) {
-				cluster1_data_ave.add(Long.parseLong(data[i][1]));
-				cluster1_data_var.add(Double.parseDouble(data[i][2]));
-			} else if (cluster_num == 2) {
-				cluster2_data_ave.add(Long.parseLong(data[i][1]));
-				cluster2_data_var.add(Double.parseDouble(data[i][2]));
-			}else if (cluster_num == 3) {
-				cluster3_data_ave.add(Long.parseLong(data[i][1]));
-				cluster3_data_var.add(Double.parseDouble(data[i][2]));
+
+			if (data[i][6].contains(comparsion_str)) {
+				count++;
+				// System.out.println(i + " : " +count);
+			}
+
+			if (count == 1) {
+				t_sum_num[t_count] = Long.parseLong(data[i][5]);
+				// System.out.println(i+" : "+t_sum_num[t_count]);
+				t_count++;
+			} else if (count == 2) {
+				udp_traffic_sum_set.add(t_sum_num);
+				// nullを消している
+				udp_traffic_sum_list = new ArrayList<Long>(Arrays.asList(t_sum_num));
+				udp_traffic_sum_list.removeAll(Collections.singleton(null));
+				udp_traffic_sum_num.add(udp_traffic_sum_list);
+				System.out.println(i + " : " + data[i][1] + " : " + udp_traffic_sum_list);
+				count = 1;
+				t_count = 0;
+				t_sum_num = new Long[udp_file_list.size()];
 			}
 		}
+		// String file_time[][] = new String[data.length][];
+
+		// for (int i = 0; i < data.length; i++) {
+		//// ip_address[i] = data[i][3];
+		// if(i>=1){
+		// //直前のIPアドレスと今のIPアドレスが同じ場合 データ量と時間を追加
+		// if(data[i][3].equals(data[i-1][3])){
+		// file_data[i][0] = ip_address[i];
+		// file_data[i][1] = data[i][3];
+		// }else{
+		//
+		// }
+		// }
+		// for(int j = 0; j < data.length; j++){
+		// for(int k = 0; k < data.length; k++){
+		//
+		// }
+		// }
+		// }
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -16,14 +16,42 @@ import java.util.regex.Pattern;
 
 public class Main {
 	// --------------ファイル読み込み用--------------
+	// オリジナルファイル(wireshark)
 	static final String file_day = "16-09-30";
-	static final String file_name = "./traffic_log/"+file_day+".csv";
-	static File arff_file = new File(file_name);
-	static ArrayList<String[]> arff_file_list = new ArrayList<String[]>();
-	static String[][] arff_data;
-	//データの平均と分散
+	static final String file_name = "./traffic_log/" + file_day + ".csv";
+	static File origin_file = new File(file_name);
+	static ArrayList<String[]> origin_file_list = new ArrayList<String[]>();
+	static String[][] origin_data;
+	// wekaの結果ファイル
+	static final String weka_file_day = "16-09-25";
+	static final String weka_file_name = "./weka_result/" + weka_file_day + ".arff";
+	static File weka_file = new File(weka_file_name);
+	static ArrayList<String[]> weka_file_list = new ArrayList<String[]>();
+	static String[][] weka_data;
+	// データの平均と分散
 	static ArrayList<Long> traffic_ave_list = new ArrayList<Long>();
 	static ArrayList<Double> traffic_var_list = new ArrayList<Double>();
+	// クラスタ0のデータの平均と分散
+	static ArrayList<Long> cluster0_data_ave = new ArrayList<Long>();
+	static ArrayList<Double> cluster0_data_var = new ArrayList<Double>();
+	static long cluster0_data_ave_result;
+	static double cluster0_data_var_result;
+	// クラスタ1のデータの平均と分散
+	static ArrayList<Long> cluster1_data_ave = new ArrayList<Long>();
+	static ArrayList<Double> cluster1_data_var = new ArrayList<Double>();
+	static long cluster1_data_ave_result;
+	static double cluster1_data_var_result;
+	// クラスタ2のデータの平均と分散
+	static ArrayList<Long> cluster2_data_ave = new ArrayList<Long>();
+	static ArrayList<Double> cluster2_data_var = new ArrayList<Double>();
+	static long cluster2_data_ave_result;
+	static double cluster2_data_var_result;
+	// クラスタ3のデータの平均と分散
+	static ArrayList<Long> cluster3_data_ave = new ArrayList<Long>();
+	static ArrayList<Double> cluster3_data_var = new ArrayList<Double>();
+	static long cluster3_data_ave_result;
+	static double cluster3_data_var_result;
+
 	// // クラスタ1のデータの平均と分散
 	// static ArrayList<Long> cluster1_data_ave = new ArrayList<Long>();
 	// static ArrayList<Double> cluster1_data_var = new ArrayList<Double>();
@@ -40,38 +68,86 @@ public class Main {
 	static ArrayList<Long> sum_list;
 	static ArrayList<ArrayList> traffic_sum_num = new ArrayList<ArrayList>();
 
+	static long ave;
+	static double var;
+
 	// メインメソッド
 	public static void main(String args[]) {
 		callFile();
-		setTrafficDataSet(arff_data);
-
+		setTrafficDataSet(origin_data);
+		DivideCluster(weka_data, weka_file_list);
 		for (int i = 0; i < traffic_sum_num.size(); i++) {
 			Average(traffic_sum_num.get(i));
-			Variance(traffic_sum_num.get(i), traffic_ave_list.get(i));
-//			System.out.println(i + " : " +traffic_sum_num.get(i));
+			traffic_ave_list.add(ave);
+			// System.out.println(i + " : " +traffic_sum_num.get(i));
 		}
-		System.out.println(traffic_ave_list.size());
-		inputFile();
+		for (int i = 0; i < traffic_sum_num.size(); i++) {
+			Variance(traffic_sum_num.get(i), traffic_ave_list.get(i));
+			traffic_var_list.add(var);
+		}
+		Average(cluster0_data_ave);
+		cluster0_data_ave_result = ave;
+		Variance(cluster0_data_ave, cluster0_data_ave_result);
+		cluster0_data_var_result = var;
+		
+		Average(cluster1_data_ave);
+		cluster1_data_ave_result = ave;
+		Variance(cluster1_data_ave, cluster1_data_ave_result);
+		cluster1_data_var_result = var;
+		
+		Average(cluster2_data_ave);
+		cluster2_data_ave_result = ave;
+		Variance(cluster2_data_ave, cluster2_data_ave_result);
+		cluster2_data_var_result = var;
+		
+		Average(cluster3_data_ave);
+		cluster3_data_ave_result = ave;
+		Variance(cluster3_data_ave, cluster3_data_ave_result);
+		cluster3_data_var_result = var;
+		
+		System.out.println(cluster0_data_ave_result+","+cluster0_data_var_result);
+		System.out.println(cluster1_data_ave_result+","+cluster1_data_var_result);
+		System.out.println(cluster2_data_ave_result+","+cluster2_data_var_result);
+		System.out.println(cluster3_data_ave_result+","+cluster3_data_var_result);
+		// System.out.println(weka_data[0][3]);
+		// createFile();
 	}
 
 	// ファイル読み込み
 	public static void callFile() {
 		try {
 			// ファイル読み込み
-			BufferedReader br = new BufferedReader(new FileReader(arff_file));
+			// オリジナルファイル
+			BufferedReader br = new BufferedReader(new FileReader(origin_file));
 			String line = br.readLine();
 			for (int row = 0; line != null; row++) {
-				arff_file_list.add(line.split(",", 0));
+				origin_file_list.add(line.split(",", 0));
 				line = br.readLine();
 			}
 			br.close();
-			arff_data = new String[arff_file_list.size()][];
+			// weka結果ファイル
+			BufferedReader br_weka = new BufferedReader(new FileReader(weka_file));
+			String line_weka = br_weka.readLine();
+			for (int row = 0; line_weka != null; row++) {
+				weka_file_list.add(line_weka.split(",", 0));
+				line_weka = br_weka.readLine();
+			}
+			br_weka.close();
+
+			origin_data = new String[origin_file_list.size()][];
+			weka_data = new String[weka_file_list.size()][];
 			// arrayListから二次元配列へ
-			for (int row = 0; row < arff_file_list.size(); row++) {
-				arff_data[row] = arff_file_list.get(row);
+			for (int row = 0; row < origin_file_list.size(); row++) {
+				origin_data[row] = origin_file_list.get(row);
 				for (int col = 0; col < 7; col++) {
-					arff_data[row][col] = arff_data[row][col].replace("\"", "");
+					origin_data[row][col] = origin_data[row][col].replace("\"", "");
 				}
+			}
+			for (int row = 0; row < weka_file_list.size(); row++) {
+				weka_data[row] = weka_file_list.get(row);
+				// for (int col = 0; col < 3; col++) {
+				// weka_data[row][col] = weka_data[row][col].replace("\"", "");
+				// }
 			}
 		} catch (IOException e) {
 			System.out.println(e);
@@ -84,19 +160,19 @@ public class Main {
 		String comparsion_str = ip_address + " is at";
 
 		//
-		traffic_data_set = new String[arff_file_list.size()];
+		traffic_data_set = new String[origin_file_list.size()];
 		int count = 0;
 		int t_count = 0;
-		Long[] t_sum_num = new Long[arff_file_list.size()];
-		for (int i = 0; i < arff_file_list.size(); i++) {
-			if (arff_data[i][6].contains(comparsion_str)) {
+		Long[] t_sum_num = new Long[origin_file_list.size()];
+		for (int i = 0; i < origin_file_list.size(); i++) {
+			if (origin_data[i][6].contains(comparsion_str)) {
 				count++;
-//				System.out.println(i + " : " +count);
+				// System.out.println(i + " : " +count);
 			}
 
 			if (count == 1) {
-				t_sum_num[t_count] = Long.parseLong(arff_data[i][5]);
-//				System.out.println(i+" : "+t_sum_num[t_count]);
+				t_sum_num[t_count] = Long.parseLong(origin_data[i][5]);
+				// System.out.println(i+" : "+t_sum_num[t_count]);
 				t_count++;
 			} else if (count == 2) {
 				traffic_sum_set.add(t_sum_num);
@@ -104,17 +180,18 @@ public class Main {
 				sum_list = new ArrayList<Long>(Arrays.asList(t_sum_num));
 				sum_list.removeAll(Collections.singleton(null));
 				traffic_sum_num.add(sum_list);
-//				System.out.println(i + " : " + arff_data[i][1]+ " : " + sum_list);
+				 System.out.println(i + " : " + origin_data[i][1]+ " : " +
+				 sum_list);
 				count = 1;
 				t_count = 0;
-				t_sum_num = new Long[arff_file_list.size()];
+				t_sum_num = new Long[origin_file_list.size()];
 			}
 		}
 	}
 
 	// 平均を求めるメソッド
 	public static void Average(ArrayList<Long> list) {
-		long ave;
+
 		long sum = 0;
 		if (list == null || list.size() == 0) {
 			// System.exit(0);
@@ -123,14 +200,14 @@ public class Main {
 				sum += list.get(i);
 			}
 			ave = sum / list.size();
-			traffic_ave_list.add(ave);
-//			System.out.println(ave);
+			// traffic_ave_list.add(ave);
+			// System.out.println(ave);
 		}
 	}
 
 	// 分散を求めるメソッド
 	public static void Variance(ArrayList<Long> list, long ave) {
-		double var;
+		// double var;
 		double sum = 0;
 		if (list == null || list.size() == 0) {
 			// System.exit(0);
@@ -139,17 +216,49 @@ public class Main {
 				sum += (double) (ave - list.get(i)) * (ave - list.get(i));
 			}
 			var = sum / list.size();
-			traffic_var_list.add(var);
-//			System.out.println(var);
+
+			// System.out.println(var);
 		}
 	}
-	
-	//ファイル作成
-	public static void inputFile() {
+
+	// 平均を求めるメソッド
+	public static void WekaAverage(ArrayList<Long> list) {
+
+		long sum = 0;
+		if (list == null || list.size() == 0) {
+			// System.exit(0);
+		} else {
+			for (int i = 0; i < list.size(); i++) {
+				sum += list.get(i);
+			}
+			ave = sum / list.size();
+			// traffic_ave_list.add(ave);
+			// System.out.println(ave);
+		}
+	}
+
+	// 分散を求めるメソッド
+	public static void WekaVariance(ArrayList<Long> list, long ave) {
+		// double var;
+		double sum = 0;
+		if (list == null || list.size() == 0) {
+			// System.exit(0);
+		} else {
+			for (int i = 0; i < list.size(); i++) {
+				sum += (double) (ave - list.get(i)) * (ave - list.get(i));
+			}
+			var = sum / list.size();
+
+			// System.out.println(var);
+		}
+	}
+
+	// ファイル作成
+	public static void createFile() {
 		Calendar myCal = Calendar.getInstance();
 		DateFormat myFormat = new SimpleDateFormat("yyyy_MM_dd_HHmm");
-		String file_csv_name = "./weka_file/"+ file_day +"_" + myFormat.format(myCal.getTime()) + ".csv";
-		String file_arff_name = "./weka_file/"+ file_day + "_" + myFormat.format(myCal.getTime()) + ".arff";
+		String file_csv_name = "./weka_file/" + file_day + "_" + myFormat.format(myCal.getTime()) + ".csv";
+		String file_arff_name = "./weka_file/" + file_day + "_" + myFormat.format(myCal.getTime()) + ".arff";
 		File new_csv_file = new File(file_csv_name);
 		File new_arff_file = new File(file_arff_name);
 		// ファイル作成
@@ -173,7 +282,6 @@ public class Main {
 			} else {
 				System.out.println("ファイルに書き込めません");
 			}
-			
 
 			// ARFFファイル作成
 			if (new_arff_file.createNewFile()) {
@@ -191,15 +299,16 @@ public class Main {
 				pw.write("\n");
 				pw.write("@data\n");
 				for (int i = 0; i < traffic_ave_list.size(); i++) {
-					pw.write(String.valueOf(traffic_ave_list.get(i)) + ","
-							+ String.valueOf(traffic_var_list.get(i)) + ",label\n");
-//					System.out.println(i + " : " + arff_data[i][1]+ " : " + sum_list);
+					pw.write(String.valueOf(traffic_ave_list.get(i)) + "," + String.valueOf(traffic_var_list.get(i))
+							+ ",label\n");
+					// System.out.println(i + " : " + origin_data[i][1]+ " : " +
+					// sum_list);
 				}
 				pw.close();
 			} else {
 				System.out.println("ファイルに書き込めません");
 			}
-			
+
 		} catch (IOException e) {
 			System.out.println(e);
 		}
@@ -214,7 +323,32 @@ public class Main {
 		}
 		return false;
 	}
-	
+
+	public static void DivideCluster(String[][] data, ArrayList list) {
+		ArrayList<String> line_data = new ArrayList<String>();
+		int cluster_num;
+		Pattern pattern;
+		Matcher matcher;
+
+		for (int i = 0; i < list.size(); i++) {
+
+			cluster_num = Integer.parseInt(data[i][3].replace("cluster", ""));
+			// System.out.println(cluster_num);
+			if (cluster_num == 0) {
+				cluster0_data_ave.add(Long.parseLong(data[i][1]));
+				cluster0_data_var.add(Double.parseDouble(data[i][2]));
+			} else if (cluster_num == 1) {
+				cluster1_data_ave.add(Long.parseLong(data[i][1]));
+				cluster1_data_var.add(Double.parseDouble(data[i][2]));
+			} else if (cluster_num == 2) {
+				cluster2_data_ave.add(Long.parseLong(data[i][1]));
+				cluster2_data_var.add(Double.parseDouble(data[i][2]));
+			} else if (cluster_num == 3) {
+				cluster3_data_ave.add(Long.parseLong(data[i][1]));
+				cluster3_data_var.add(Double.parseDouble(data[i][2]));
+			}
+		}
+	}
 }
 
 // クラスタごとに平均、分散を分ける
@@ -223,7 +357,7 @@ public class Main {
 // int cluster_num;
 // Pattern pattern;
 // Matcher matcher;
-// for (int i = 0; i < arff_file_list.size(); i++) {
+// for (int i = 0; i < origin_file_list.size(); i++) {
 // cluster_num = Integer.parseInt(data[i][3].replace("cluster", ""));
 //// System.out.println(cluster_num);
 // if (cluster_num == 0) {
